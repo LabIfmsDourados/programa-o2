9417d3cd4b5cd20ca7bf99cc9cdef14b84590d90
LabIfmsDourados <60228821+LabIfmsDourados@users.noreply.github.com>
2022-02-24 20:07:14 -0300

    primeiro commit

---
+++ Estudo/Estudo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
