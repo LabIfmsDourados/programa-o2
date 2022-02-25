e1b3d03caa9fcf90eb1df3ab99bb02cdfbb7e71b
LabIfmsDourados <60228821+LabIfmsDourados@users.noreply.github.com>
2022-02-24 22:14:32 -0300

    class

--- Estudo/Estudo/Program.cs
+++ Estudo/Estudo/Program.cs
@@ -2,11 +2,70 @@
 
 namespace Estudo
 {
-    class Program
+    public class Energia
     {
+       public string energiza;
+    }
+    public class TesteLampada : Energia
+    {
+        static bool valorAtual;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Energia conta = new Energia();
+
+
+
+            conta.energiza = "André";
+
+            Aluno atributos = new Aluno();
+            Aluno Andre = new Aluno();
+            Aluno Antonio = new Aluno();
+            Program lampada = new Program();
+            //valorAtual = lampada.verEstadoLampada();
+            //Console.WriteLine(valorAtual);
+            lampada.AcenderLampada();
+            Console.WriteLine(lampada.verEstadoLampada());
+            lampada.ApagarLampada();
+            Console.WriteLine(lampada.verEstadoLampada());
+            atributos.idade = 20;
+            atributos.nota = 10;
+            Console.WriteLine("Nota " + atributos.nota);
+            Antonio.idade = 21;
+            Antonio.nota = 10;
+            Antonio.aprovado = true;
+
+            Andre.idade = 24;
+            Andre.nota = 4;
+            Andre.aprovado = false;
+
+            Console.WriteLine(Antonio.idade +" "+ Antonio.aprovado + " " + Andre.idade + " " + Andre.aprovado);
         }
+    }
+
+    class Program
+    {
+        public bool estadoLampada = false;
+        public void AcenderLampada()
+        {
+            estadoLampada = true;
+        }
+        public void ApagarLampada()
+        {
+            estadoLampada = false;
+        }
+        public bool verEstadoLampada()
+        {
+            return estadoLampada;
+        }
+        public void getMatriz()
+        {
+
+        }
+    }
+    class Aluno
+    {
+       public string nome;
+       public int idade, nota, ano;
+        public bool aprovado;
     }
 }
